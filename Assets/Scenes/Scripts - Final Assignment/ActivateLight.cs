88ea6931faa07e52e5ed55a72a13f26f2a99811c
Anita Hosseini <hosseani@shernet.sheridancollege.ca>
2026-04-15 13:45:06 -0400

    reworked seeker's behaviour. It actually goes on it's patrol path now!

--- Assets/Scenes/Scripts - Final Assignment/ActivateLight.cs
+++ Assets/Scenes/Scripts - Final Assignment/ActivateLight.cs
@@ -1,27 +1,25 @@
 using UnityEngine;
 
-public class DeactivateLights : MonoBehaviour
+public class ActivateLight : MonoBehaviour
 {
     Collider col;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        col = gameObject.GetComponent<Collider>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
-
     private void OnTriggerEnter(Collider other)
     {
         print("entering");
         if (other.gameObject.CompareTag("Generator"))
         {
-            other.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+            other.gameObject.transform.GetChild(0).gameObject.SetActive(true);
         }
     }
 }
